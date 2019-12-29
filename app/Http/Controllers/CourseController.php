b234b5df367600c01054425a30d7bb4f07009d13
KaungKaung0 <kaungsan206@gmail.com>
2019-12-29 17:05:18 +0630

    Single Course Page Added

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Course;
 class CourseController extends Controller
 {
     //
     public function index($id){
-    	dd($id);
+    	$data = Course::where('id', $id)->first();
+    	return view('course' , compact('data'));
     }
 }
