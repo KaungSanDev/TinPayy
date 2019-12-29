310067270e5f3f9bde777457eabd411ece6f0413
KaungKaung0 <kaungsan206@gmail.com>
2019-12-29 17:18:37 +0630

    pic fix

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -29,7 +29,7 @@
             <!-- Logo -->
             <div class="logo_container">
                 <div class="logo">
-                    <img src="images/logo.png" alt="">
+                    <img src="{{secure_asset('images/logo.png')}}" alt="">
                     <span>course</span>
                 </div>
             </div>
@@ -103,7 +103,7 @@
 
     <div class="home">
         <div class="home_background_container prlx_parent">
-            <div class="home_background prlx" style="background-image:url(images/news_background.jpg)"></div>
+            <div class="home_background prlx" style="background-image:url('https://source.unsplash.com/iWTamkU5kiI')"></div>
         </div>
         <div class="home_content">
             <h1>Course</h1>
@@ -469,6 +469,6 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
 <script src="{{secure_asset('plugins/scrollTo/jquery.scrollTo.min.js')}}"></script>
 <script src="{{secure_asset('plugins/easing/easing.js')}}"></script>
 <script src="{{secure_asset('js/custom.js')}}"></script>
-<script src="{{secure_asset('js/news_post_custom.js')}}"></script>
+<script src="{{secure_assset('js/news_post_custom.js')}}"></script>
 </body>
 </html>
